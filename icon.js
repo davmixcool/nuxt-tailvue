668fe852e44beb2918dc5509f4c6de1eb4688ebe
David Oti <davmixcool@gmail.com>
2022-12-07 00:23:59 +0100

    Update icon.js

--- icon.js
+++ icon.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import TvIcon from 'tv-icon'
+import { IconBang, IconCheck, IconInfo, IconTimes, IconSpinner, IconQuestion } from 'tv-icon'
 import 'tv-icon/dist/tv-icon.css'
-Vue.use(TvIcon)
+Vue.use(IconBang)
+Vue.use(IconCheck)
+Vue.use(IconInfo)
+Vue.use(IconTimes)
+Vue.use(IconSpinner)
+Vue.use(IconQuestion)
